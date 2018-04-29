ac22f6430bfc799bdd8ce0f0c487a818bbea4800
jasonhavenD <jasonhaven@163.com>
2018-04-29 22:54:03 +0800

    update

--- blog_project/static/js/kindeditor-4.1.10/config.js
+++
@@ -1,7 +0,0 @@
-KindEditor.ready(function (K) {
-    K.create('textarea[name=content]', {/*css元素*/
-        width: '800px',
-        height: '200px',
-        uploadJson: '/admin/upload/kindeditor',/*请求url*/
-    });
-});
